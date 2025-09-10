29d497fd863fd6700b22b040a10ab98151a487b5
SARIFUDDIN <sarifuddinpelajar@gmail.com>
2025-09-10 21:11:00 +0800

    Create day3.java

---
+++ day3.java
@@ -0,0 +1,31 @@
+/**
+ * Hari ke-3 go 100
+ * Fokus: nyobain komentar di Java
+ * Ternyata komentar nggak bikin program jalan, cuma buat catatan aja.
+ */
+package com.mycompany.day3.java;
+public class Day3Java {
+
+    public static void main(String[] args) {
+      // Oke, coba dulu komentar satu baris.
+        // Kalau kayak gini ya jelas nggak dieksekusi.
+        System.out.println("Tes komentar satu baris berhasil!");
+        
+        /*
+          Nah ini komentar multi-baris.
+          Enak dipakai kalau mau nulis panjang.
+          Misalnya lagi mikirin ide:
+          - Mau bikin input nama user
+          - Mau tambahin perhitungan sederhana
+          Tapi ya itu, masih catatan aja dulu.
+        */
+        System.out.println("Tes komentar multi-baris berhasil!");
+        
+        // Baris ini sengaja saya matiin biar nggak jalan dulu:
+        // System.out.println("Harusnya ini nggak keluar di console");
+        
+        // Udah cukup lah. Jadi intinya komentar itu buat kita sendiri,
+        // biar kalau buka lagi kode ini, nggak bingung.
+        System.out.println("Program selesai. Belajar komentar aman!");
+    }
+}
