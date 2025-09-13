391a18815677495909c154a775eb42909139eb45
SARIFUDDIN <sarifuddinpelajar@gmail.com>
2025-09-13 14:47:45 +0800

    Day5.java

---
+++ Day5.java
@@ -0,0 +1,13 @@
+public class Day5 {
+    public static void main(String[] args) {
+        byte b = 127;
+        short s = 32000;
+        int i = 1000000000;
+        long l = 10000000000L;
+
+        System.out.printf("Byte   : %d\n", b);
+        System.out.printf("Short  : %d\n", s);
+        System.out.printf("Int    : %d\n", i);
+        System.out.printf("Long   : %d\n", l);
+    }
+}
