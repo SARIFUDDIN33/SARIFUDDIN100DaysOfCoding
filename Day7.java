71e0dbfb9595d01f467807b684bd0f59684e8233
SARIFUDDIN <sarifuddinpelajar@gmail.com>
2025-09-13 14:52:17 +0800

    Day7.java

---
+++ Day7.java
@@ -0,0 +1,10 @@
+//Tipe Data Karakter (char) dan Boolean (boolean).
+public class Day7 {
+    public static void main(String[] args) {
+        boolean benar = true;
+        boolean salah = false;
+
+        System.out.println("Nilai benar: " + benar);
+        System.out.println("Nilai salah: " + salah);
+    }
+}
