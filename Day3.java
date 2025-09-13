337bd2848e023d5866ecd0f9e9bb04a20d31a8b7
SARIFUDDIN <sarifuddinpelajar@gmail.com>
2025-09-13 15:03:29 +0800

    Day3.java

---
+++ Day3.java
@@ -0,0 +1,14 @@
+public class Day3 {
+    public static void main(String[] args) {
+        // komentar satu baris
+        System.out.println("Tes komentar satu baris berhasil!");
+
+        /*
+          komentar multi-baris
+          bisa untuk catatan panjang
+        */
+        System.out.println("Tes komentar multi-baris berhasil!");
+
+        // System.out.println("Baris ini dimatikan");
+    }
+}
