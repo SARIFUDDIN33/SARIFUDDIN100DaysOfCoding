4881ede504397a1be09650c827bd4919173a7b59
SARIFUDDIN <sarifuddinpelajar@gmail.com>
2025-09-13 14:49:30 +0800

    Day6.java

---
+++ Day6.java
@@ -0,0 +1,9 @@
+public class Day6 {
+    public static void main(String[] args) {
+        float f = 3.14f;
+        double d = 3.14159265359;
+
+        System.out.printf("Float  : %.2f\n", f);
+        System.out.printf("Double : %.10f\n", d);
+    }
+}
