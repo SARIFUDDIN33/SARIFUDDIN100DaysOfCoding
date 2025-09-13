7b1b554e678fa15faaf65d1ada360d74df3e314d
SARIFUDDIN <sarifuddinpelajar@gmail.com>
2025-09-13 15:00:36 +0800

    Day12.java

---
+++ Day12.java
@@ -0,0 +1,11 @@
+public class Day12 {
+    public static void main(String[] args) {
+        int x = 10;
+
+        x += 5;  // sama dengan x = x + 5
+        x -= 2;  // sama dengan x = x - 2
+        x *= 3;  // sama dengan x = x * 3
+
+        System.out.println("Hasil akhir: " + x);
+    }
+}
