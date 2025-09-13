c3c1341fdc6114106da59f8517b2cb1691def658
SARIFUDDIN <sarifuddinpelajar@gmail.com>
2025-09-13 14:57:03 +0800

    Day8.java

---
+++ Day8.java
@@ -0,0 +1,9 @@
+public class Day8 {
+    public static void main(String[] args) {
+        char huruf = 'A';    // karakter tunggal pakai tanda petik satu
+        char simbol = '#';   // bisa juga simbol
+
+        System.out.println("Huruf: " + huruf);
+        System.out.println("Simbol: " + simbol);
+    }
+}
