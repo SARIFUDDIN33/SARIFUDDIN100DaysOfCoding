bb66a43756531b3f96caee79a2c8ae28091d6724
SARIFUDDIN <sarifuddinpelajar@gmail.com>
2025-09-13 14:58:02 +0800

    Day9.java

---
+++ Day9.java
@@ -0,0 +1,8 @@
+public class Day9 {
+    public static void main(String[] args) {
+        String nama = "Sarifuddin";         // teks panjang
+        String sapaan = "Halo, " + nama;    // gabung string pakai +
+
+        System.out.println(sapaan);
+    }
+}
