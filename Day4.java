ff1e3bfdefb4c3fdd09522925654f061e3249cef
SARIFUDDIN <sarifuddinpelajar@gmail.com>
2025-09-13 14:44:59 +0800

    Update Day4.java

--- Day4.java
+++ Day4.java
@@ -3,19 +3,7 @@ package com.mycompany.day4.java;
 public class Day4Java {
 
     public static void main(String[] args) {
-        System.out.println("Halo, ini baris pertama\nIni baris kedua\nIni baris ketiga");
-
-        // \t = tab / jarak spasi lebih panjang
-        System.out.println("Nama:\tSarifuddin");
-        System.out.println("Kelas:\tA Informatika");
-
-        // \" = biar bisa nampilin tanda kutip di dalam teks
-        System.out.println("Dia bilang: \"Belajar Java itu seru!\"");
-
-        // \\ = buat nampilin backslash
-        System.out.println("Lokasi folder: C:\\Users\\Sarif");
-
-        // Gabungan escape sequence (biar lebih keliatan bedanya)
-        System.out.println("Daftar Belanja:\n\t- Kopi\n\t- Gula\n\t- Susu");
-    }
-}
+        System.out.println("Hello\nWorld");
+        System.out.println("Hello\tWorld");
+        System.out.println("\"Hello World\"");
+        System.out.println("Hello\\World");
