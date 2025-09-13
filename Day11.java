d9fac874cc6b78588ce8cb90f67dc76da93b8f7c
SARIFUDDIN <sarifuddinpelajar@gmail.com>
2025-09-13 14:59:30 +0800

    Day11.java

---
+++ Day11.java
@@ -0,0 +1,12 @@
+public class Day11 {
+    public static void main(String[] args) {
+        int x = 5, y = 7;
+
+        System.out.println(x == y);  // sama dengan?
+        System.out.println(x != y);  // tidak sama dengan?
+        System.out.println(x > y);   // lebih besar?
+        System.out.println(x < y);   // lebih kecil?
+        System.out.println(x >= y);  // lebih besar atau sama?
+        System.out.println(x <= y);  // lebih kecil atau sama?
+    }
+}
