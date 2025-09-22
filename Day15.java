27665defc7beb771bea491709b05e710b1af2d71
SARIFUDDIN <sarifuddinpelajar@gmail.com>
2025-09-22 21:42:47 +0800

    Create Day15.java

---
+++ Day15.java
@@ -0,0 +1,14 @@
+public class Day15 {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 5;
+
+        // Penjumlahan
+        int hasilTambah = a + b;
+        System.out.println(a + " + " + b + " = " + hasilTambah);
+
+        // Pengurangan
+        int hasilKurang = a - b;
+        System.out.println(a + " - " + b + " = " + hasilKurang);
+    }
+}
