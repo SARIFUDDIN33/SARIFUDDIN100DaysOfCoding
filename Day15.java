a07322201dfaee615de7ddf5327034a4540f53fb
SARIFUDDIN <sarifuddinpelajar@gmail.com>
2025-09-22 21:48:10 +0800

    Update Day15.java

--- Day15.java
+++ Day15.java
@@ -1,14 +1,16 @@
 public class Day15 {
     public static void main(String[] args) {
-        int a = 10;
-        int b = 5;
+        int angka1 = 12;
+        int angka2 = 7;
 
-        // Penjumlahan
-        int hasilTambah = a + b;
-        System.out.println(a + " + " + b + " = " + hasilTambah);
+        System.out.println("=== Operasi Aritmatika ===");
+        System.out.println("Angka pertama : " + angka1);
+        System.out.println("Angka kedua   : " + angka2);
 
-        // Pengurangan
-        int hasilKurang = a - b;
-        System.out.println(a + " - " + b + " = " + hasilKurang);
+        int tambah = angka1 + angka2;
+        int kurang = angka1 - angka2;
+
+        System.out.println("Hasil Penjumlahan = " + tambah);
+        System.out.println("Hasil Pengurangan = " + kurang);
     }
 }
