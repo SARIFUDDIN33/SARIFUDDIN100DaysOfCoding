306ab459ba3372a1182f3a08b6e06d3deb431550
SARIFUDDIN <sarifuddinpelajar@gmail.com>
2025-09-22 22:01:36 +0800

    Create Day15.java

---
+++ Day15.java
@@ -0,0 +1,22 @@
+package minggu02;
+import java.util.Scanner;
+
+public class Day15 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+      
+        System.out.print("Masukkan angka1: ");
+        int angka1 = input.nextInt();
+        
+        System.out.print("Masukkan angka2: ");
+        int angka2 = input.nextInt();
+        
+        int jumlah = angka1 + angka2;
+        int selisih = angka1 - angka2;
+        
+        System.out.println("Hasil Penjumlahan = " + jumlah);
+        System.out.println("Hasil Pengurangan = " + selisih);
+        
+        
+    }
+}
