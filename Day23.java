cdbc5bb1de6f6ddae771a84fa060055c4fff8564
SARIFUDDIN <sarifuddinpelajar@gmail.com>
2025-09-30 19:15:50 +0800

    Create Day23.java

---
+++ Day23.java
@@ -0,0 +1,16 @@
+package minggu2;
+public class Day23 {
+    public static void main(String[] args) {
+        // Deklarasi variabel
+        int panjang = 8;
+        int lebar = 4;
+
+        // Hitung luas persegi panjang
+        int luas = panjang * lebar;
+
+        // Output hasil
+        System.out.println("Panjang = " + panjang);
+        System.out.println("Lebar = " + lebar);
+        System.out.println("Luas persegi panjang = " + luas);
+    }
+}
