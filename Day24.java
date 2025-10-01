4d8ffc07698a17b014c2e6ddac4b5d6e828acc5d
SARIFUDDIN <sarifuddinpelajar@gmail.com>
2025-10-01 22:32:18 +0800

    Create Day24.java

---
+++ Day24.java
@@ -0,0 +1,16 @@
+package minggu2;
+
+public class Day24 {
+    public static void main(String[] args) {
+        // Deklarasi variabel
+        double jariJari = 7;
+        double phi = 3.14;
+
+        // Hitung luas lingkaran
+        double luas = phi * jariJari * jariJari;
+
+        // Output hasil
+        System.out.println("Jari-jari = " + jariJari);
+        System.out.println("Luas lingkaran = " + luas);
+    }
+}
