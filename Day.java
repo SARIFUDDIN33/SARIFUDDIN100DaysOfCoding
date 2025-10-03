3c34e1aa084238f4925353839f7d7076f37f6783
SARIFUDDIN <sarifuddinpelajar@gmail.com>
2025-10-03 23:30:37 +0800

    Create Day.java

---
+++ Day.java
@@ -0,0 +1,18 @@
+package minggu2;
+
+public class Day26 {
+    public static void main(String[] args) {
+        // Deklarasi variabel
+        int angka = 10;
+
+        // Operator penugasan
+        angka += 5;   // sama dengan angka = angka + 5
+        angka -= 3;   // sama dengan angka = angka - 3
+        angka *= 2;   // sama dengan angka = angka * 2
+        angka /= 4;   // sama dengan angka = angka / 4
+        angka %= 3;   // sama dengan angka = angka % 3
+
+        // Output hasil
+        System.out.println("Hasil akhir angka = " + angka);
+    }
+}
