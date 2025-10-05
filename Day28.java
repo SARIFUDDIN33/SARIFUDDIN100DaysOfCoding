49daebdfc6e55d49b5c1d6132091d3bc31df48d2
SARIFUDDIN <sarifuddinpelajar@gmail.com>
2025-10-05 23:37:14 +0800

    Create Day28.java

---
+++ Day28.java
@@ -0,0 +1,19 @@
+package minggu3;
+
+public class Day28 {
+    public static void main(String[] args) {
+        // Deklarasi variabel
+        int a = 10;
+        int b = 20;
+
+        // Operator perbandingan
+        boolean hasilSama = (a == b);   // Apakah a sama dengan b?
+        boolean hasilTidakSama = (a != b); // Apakah a tidak sama dengan b?
+
+        // Output hasil
+        System.out.println("Nilai a = " + a);
+        System.out.println("Nilai b = " + b);
+        System.out.println("Apakah a == b? " + hasilSama);
+        System.out.println("Apakah a != b? " + hasilTidakSama);
+    }
+}
