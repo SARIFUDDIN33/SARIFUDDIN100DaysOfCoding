11cf01c1f1417366b4a3981014571cc21968062e
SARIFUDDIN <sarifuddinpelajar@gmail.com>
2025-10-06 23:09:09 +0800

    Create Day29.java

---
+++ Day29.java
@@ -0,0 +1,19 @@
+package minggu3;
+
+public class Day29 {
+    public static void main(String[] args) {
+        // Deklarasi variabel
+        int a = 15;
+        int b = 10;
+
+        // Operator perbandingan < dan >
+        boolean hasilLebihKecil = (a < b);  // Apakah a lebih kecil dari b?
+        boolean hasilLebihBesar = (a > b);  // Apakah a lebih besar dari b?
+
+        // Output hasil
+        System.out.println("Nilai a = " + a);
+        System.out.println("Nilai b = " + b);
+        System.out.println("Apakah a < b? " + hasilLebihKecil);
+        System.out.println("Apakah a > b? " + hasilLebihBesar);
+    }
+}
