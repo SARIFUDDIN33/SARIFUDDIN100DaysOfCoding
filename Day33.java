3832bb38e3474a340e5978c71809dd61fe2f1cd4
SARIFUDDIN <sarifuddinpelajar@gmail.com>
2025-10-10 23:27:05 +0800

    Create Day33.java

---
+++ Day33.java
@@ -0,0 +1,11 @@
+package minggu3;
+
+public class Day33 {
+    public static void main(String[] args) {
+        boolean status = true;
+        boolean hasil = !status;
+
+        System.out.println("Status awal: " + status);
+        System.out.println("Setelah dibalik dengan operator NOT: " + hasil);
+    }
+}
